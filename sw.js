13dd39f4e6f7e5ccb155990a8ab06442d43563ed
Thomas Vahldick Kutscher <caiostein@192.168.68.109>
2026-09-17 14:05:36 -0300

    Initial commit: HAZSA workout tracker PWA
    Single-file offline-first workout tracking app with a service worker
    for offline caching once hosted.

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'hazsa-v1';
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=> c.addAll(['./', './index.html'])));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=> Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      const fetchPromise = fetch(e.request).then(res=>{
+        caches.open(CACHE).then(c=> c.put(e.request, res.clone()));
+        return res;
+      }).catch(()=> cached);
+      return cached || fetchPromise;
+    })
+  );
+});
